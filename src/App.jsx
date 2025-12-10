edd38118ad9e7ed74a951cd27c61552094d24c52
ChuHuy2208 <chuquanghuy2208@gmail.com>
2025-12-10 11:41:17 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+
+function App() {
+
+  return (
+    <>
+      <Typography variant='body2' color='text.secondary'>Test</Typography>
+    </>
+  )
+}
+
+export default App
