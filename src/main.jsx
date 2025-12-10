8496ceb7972740b834e04c48f8f4bf50cab51813
ChuHuy2208 <chuquanghuy2208@gmail.com>
2025-12-10 21:19:29 +0700

    basic-layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
-import theme from '../theme.js'
+import theme from './theme.js'
 import CssBaseline from '@mui/material/CssBaseline'
 
 createRoot(document.getElementById('root')).render(
